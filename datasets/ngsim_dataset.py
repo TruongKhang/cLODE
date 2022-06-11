02e86b0fb018db42523e97b8afa0de23cb71eab7
TruongKhang <truongkhang95@gmail.com>
2022-06-11 18:28:41 +0900

    update

--- datasets/ngsim_dataset.py
+++ datasets/ngsim_dataset.py
@@ -5,7 +5,6 @@ from torch.utils.data import Dataset
 
 
 import datasets.utils as utils
-np.random.seed(1995)
 
 
 NGSIM_FILENAME_TO_ID = {
@@ -65,7 +64,7 @@ class NGSIMDataset(Dataset):
         observations = self.all_traj_data[self.mapping_idx[idx]]
 
         time_steps = np.where(np.sum(np.abs(observations), axis=1) > 0)[0]
-        n_sample_ts = int(np.random.rand() * len(time_steps)) + 4 if self.max_obs_length is not None else self.max_obs_length
+        n_sample_ts = int(np.random.rand() * len(time_steps)) + 4 if self.max_obs_length is None else self.max_obs_length
         # if self.mode == 'test':
         #     start_ts = np.random.randint(len(time_steps) - self.max_obs_length)
         # else:
